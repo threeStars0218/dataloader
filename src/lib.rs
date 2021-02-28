fd20c9145fcd104a4f46dee06ee68bb4f0a064ef
ryotaro.mitsuboshi <2ie18026e@s.kyushu-u.ac.jp>
2021-02-28 16:34:50 +0900

    Implement read_csv for dense data

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,113 @@
+// use std::process;
+use std::env;
+use std::collections::HashMap;
+use std::error::Error;
+use std::fs::File;
+use std::io::prelude::*;
+
 #[cfg(test)]
 mod tests {
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn data_new_test() {
+        use crate::Data;
+        let s = "1.0,2.1,0.3,0.0";
+        let data = match Data::new(s){
+            Data::Dense(v) => v,
+            _ => vec![],
+        };
+        let ans = vec![1.0, 2.1, 0.3, 0.0];
+        assert_eq!(data, ans);
     }
+}
+
+
+#[derive(Debug)]
+enum FileKind {
+    CSV,
+    LIBSVM,
+}
+
+impl FileKind {
+    fn new(filename: &str) -> Option<FileKind> {
+
+        let pos = match filename.rfind('.') {
+            Some(p) => p,
+            None => return None,
+        };
+
+        let kind = match &filename[pos..] {
+            ".csv" => FileKind::CSV,
+            ".libsvm" => FileKind::LIBSVM,
+            _ => return None,
+        };
+
+        Some(kind)
+    }
+}
+
+
+#[derive(Debug)]
+pub struct Config {
+    kind: FileKind,
+    filename: String,
+}
+
+impl Config {
+    pub fn new(filename: String) -> Result<Config, &'static str> {
+
+        let kind = match FileKind::new(&filename) {
+            Some(k) => k,
+            None => return Err("No extension"),
+        };
+
+        Ok(Config{ kind, filename })
+    }
+
+}
+
+// I'll implement these enum/struct to general type, some day.
+
+#[derive(Debug)]
+pub enum Data {
+    Dense(Vec<f64>),
+    Sparse(HashMap<u64, f64>),
+}
+
+impl Data {
+    pub fn new(line: &str) -> Data {
+        let data = line.split(',')
+                       .map(|s| s.parse().unwrap())
+                       .collect();
+        Data::Dense(data)
+    }
+}
+
+
+pub struct SupervisedData {
+    pub label: i32,
+    pub data: Data,
+}
+
+fn read_csv(filename: &String) -> Result<Vec<Data>, Box<Error>> {
+    let mut f = File::open(filename)?;
+
+    let mut contents = String::new();
+    f.read_to_string(&mut contents)?;
+    let results = contents.lines()
+                          .map(|line| Data::new(line))
+                          .collect();
+    Ok(results)
+}
+
+pub fn read(mut s: String) -> Result<Vec<Data>, Box<Error>> {
+    let config = Config::new(s)?;
+
+    /*
+    match config.kind {
+        FileKind::CSV => read_csv(&config.filename),
+        // FileKind::LIBSVM => read_libsvm(config.filename),
+        _ => Err(Box::new("File format error")),
+    }
+    */
+    read_csv(&config.filename)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,61 +1,23 @@
+extern crate dataloader;
+
 use std::env;
 use std::process;
 
-#[derive(Debug)]
-enum FileKind {
-    CSV,
-    LIBSVM,
-}
-
-impl FileKind {
-    fn new(filename: &str) -> Option<FileKind> {
-
-        let pos = match filename.rfind('.') {
-            Some(p) => p,
-            None => return None,
-        };
-
-        let kind = match &filename[pos..] {
-            ".csv" => FileKind::CSV,
-            ".libsvm" => FileKind::LIBSVM,
-            _ => return None,
-        };
-
-        Some(kind)
-    }
-}
-
-#[derive(Debug)]
-pub struct Config {
-    kind: FileKind,
-    filename: String,
-}
-
-impl Config {
-    pub fn new(mut args: std::env::Args) -> Result<Config, &'static str> {
-        args.next();
-
-        let filename = match args.next() {
-            Some(arg) => arg,
-            None => return Err("No file specified"),
-        };
-
-        let kind = match FileKind::new(&filename) {
-            Some(k) => k,
-            None => return Err("No extension"),
-        };
-
-        Ok(Config{ kind, filename })
-    }
-}
+use dataloader::read;
 
 fn main() {
 
-    let config = Config::new(env::args()).unwrap_or_else(|err| {
-        eprintln!("Problem parsing arguments: {}", err);
-        process::exit(1);
-    });
+//     let config = Config::new(env::args()).unwrap_or_else(|err| {
+//         eprintln!("Problem parsing arguments: {}", err);
+//         process::exit(1);
+//     });
+// 
+//     println!("{:?}", config);
 
+    let filename = String::from("test.csv");
+    let v = read(filename);
+    for data in v.iter() {
+        println!("{:?}", data);
+    }
     println!("Hello, world!");
-    println!("{:?}", config);
 }
